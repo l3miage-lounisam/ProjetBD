08b927132a60dbb0be323b8086b964362405c184
salima BOUZERA <77328590+l3miage-bouzeras@users.noreply.github.com>
2022-03-27 20:31:21 +0200

    Update Definition.java
    Commentaires

--- src/main/java/l3miage/model/Definition.java
+++ src/main/java/l3miage/model/Definition.java
@@ -1,6 +1,11 @@
 package l3miage.model;
 
 /**
+ * creation d'une enumération Définition permet de determiner si une station 
+ * est Vplus(c'est a dire une station ou il y'a de l'emplacement libre par exemeple ) et une 
+ * Station Vmoins(la présense de vélo disponible)ou Vnul (sans effet)
+ * par exemple, vers le campus universitaire le matin(ici le campus est Vplus), et depuis le campus universitaire le soir
+universitaire le matin, et depuis le campus universitaire le soir(ici il est Vmoins)
  * 
  */
 public enum Definition {
