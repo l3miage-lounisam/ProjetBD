3506e1e54c78cc01d8a1a385f09bfad9e30a0cf6
salima BOUZERA <77328590+l3miage-bouzeras@users.noreply.github.com>
2022-03-27 20:34:11 +0200

    Update Etat.java
    Commentaires

--- src/main/java/l3miage/model/Etat.java
+++ src/main/java/l3miage/model/Etat.java
@@ -1,7 +1,7 @@
 package l3miage.model;
 
 /**
- * création de l'enumération Etat qui determine l'etat d'un velo(en panne"hs" ou pas "ok") et 
+ * création d'une enumération Etat qui determine l'etat d'un velo(en panne"hs" ou pas "ok") et 
  * d'une bornette(peut acceuillir un vélo"ok" ou pas "hs")
  * 
  * 
